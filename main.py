91255dfc7ee8209d02b6ec0830179e44d44e6447
Ana Cabrera <ana.cabrera@veevart.com>
2019-05-15 16:59:02 -0500

    Class 3.5

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+import unittest
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 
 todos = ['Buy coffee', 'Buy game', 'Buy milk ']
+
+@app.cli.command()
+def test():
+  tests = unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner().run(tests)
+
+@app.errorhandler(404)
+def not_found(error):
+  return render_template('404.html', error=error)
 
 @app.route('/')
 def index():
   user_ip = request.remote_addr
 
   response = make_response(redirect('/hello'))
-  response.set_cookie('user_ip', user_ip)
-
+  session['user_ip'] = user_ip
+  
   return response
 
 
 @app.route('/hello')
 def hello():
-  user_ip = request.cookies.get('user_ip')
+  user_ip = session.get('user_ip')
   context = {
     'user_ip':user_ip,
     'todos':todos
   }
+
   return render_template('hello.html', **context)
